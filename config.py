f3393fa5379083cb00712ef8e87a5771eafcaa38
tonuimartin <martinkirui57@gmail.com>
2024-12-20 00:04:38 +0300

    configuration file commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# Model parameters
+IMG_HEIGHT = 224
+IMG_WIDTH = 224
+BATCH_SIZE = 32
+EPOCHS = 20
+LEARNING_RATE = 1e-4
+
+# Data parameters
+TRAIN_PATH = 'data/train'
+TEST_PATH = 'data/test'
+VALIDATION_SPLIT = 0.2
