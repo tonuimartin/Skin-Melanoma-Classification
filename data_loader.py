ca851fe3d963fb6cfd745833f5dfd7d094025b30
tonuimartin <martinkirui57@gmail.com>
2024-12-20 17:11:49 +0300

    data loader file

---
+++ data_loader.py
@@ -0,0 +1,44 @@
+from keras_preprocessing.image import ImageDataGenerator
+from config import *
+
+def create_data_generators():
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True,
+        vertical_flip=True,
+        fill_mode='nearest',
+        validation_split=VALIDATION_SPLIT
+    )
+    
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    
+    return train_datagen, test_datagen
+
+def load_data(train_datagen, test_datagen):
+    train_generator = train_datagen.flow_from_directory(
+        TRAIN_PATH,
+        target_size=(IMG_HEIGHT, IMG_WIDTH),
+        batch_size=BATCH_SIZE,
+        class_mode='binary',
+        subset='training'
+    )
+    
+    validation_generator = train_datagen.flow_from_directory(
+        TRAIN_PATH,
+        target_size=(IMG_HEIGHT, IMG_WIDTH),
+        batch_size=BATCH_SIZE,
+        class_mode='binary',
+        subset='validation'
+    )
+    
+    test_generator = test_datagen.flow_from_directory(
+        TEST_PATH,
+        target_size=(IMG_HEIGHT, IMG_WIDTH),
+        batch_size=BATCH_SIZE,
+        class_mode='binary'
+    )
+    
+    return train_generator, validation_generator, test_generator
