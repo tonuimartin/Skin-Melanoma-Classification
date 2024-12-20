25ca58e36f632c0b318c4ea96b8eb148de0332d6
tonuimartin <martinkirui57@gmail.com>
2024-12-20 17:29:38 +0300

    model.py creating with base model as ResNet50 and a trainable model

---
+++ model.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from keras import layers, models
+from  keras_applications import ResNet50
+from config import *
+
+def create_model():
+    base_model = ResNet50(
+        weights='imagenet',
+        include_top=False,
+        input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)
+    )
+    
+    base_model.trainable = False
+    
+    model = models.Sequential([
+        base_model,
+        layers.GlobalAveragePooling2D(),
+        layers.Dense(256, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(1, activation='sigmoid')
+    ])
+    
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),
+        loss='binary_crossentropy',
+        metrics=['accuracy', tf.keras.metrics.AUC()]
+    )
+    
+    return model
